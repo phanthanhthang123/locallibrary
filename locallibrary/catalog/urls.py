4688cbed8cfe38a4f53f4f16d6f9f1a38f3c5a73
Thang.pt <thang.pt225927@sis.hust.edu.vn>
2025-07-14 10:25:19 +0700

    Django Tutorial Part5: Creating our home page

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from django.conf.urls.i18n import set_language
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),  # Home page
+    path('i18n/setlang/', set_language, name='set_language'),
+]
