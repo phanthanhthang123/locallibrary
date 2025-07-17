3b2486fcf9ec41ec1e5064f86045298718191d2c
Thang.pt <thang.pt225927@sis.hust.edu.vn>
2025-07-17 09:37:21 +0700

    Django Tutorial Part6: Generic list and detail views

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Home page
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('i18n/setlang/', set_language, name='set_language'),
 ]
